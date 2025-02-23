423427abb7335c00260a5dae81858b91d1bc35b9
YOUNES EL BANDKI <youneselbandki0@gmail.com>
2025-02-23 21:55:11 +0100

    init commit

---
+++ src/main/java/com/miniprojetspring/MiniProjetSpringApplication.java
@@ -0,0 +1,13 @@
+package com.miniprojetspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjetSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProjetSpringApplication.class, args);
+    }
+
+}
