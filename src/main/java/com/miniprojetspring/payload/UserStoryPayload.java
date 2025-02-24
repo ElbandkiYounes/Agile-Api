a523f9aca4201fa112b780f4b2b581ae970b4028
mouad <mouadofox88@gmail.com>
2025-02-24 12:43:31 +0000

    Implement UserStory model, repository, and service with priority and status enums

---
+++ src/main/java/com/miniprojetspring/payload/UserStoryPayload.java
@@ -0,0 +1,28 @@
+package com.miniprojetspring.payload;
+
+import com.miniprojetspring.Model.UserStoryPriority;
+import com.miniprojetspring.Model.UserStoryStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserStoryPayload {
+
+    @NotBlank(message = "Title cannot be blank")
+    private String title;
+
+    @NotBlank(message = "Description cannot be blank")
+    private String description;
+
+    @NotBlank(message = "Product Backlog ID is required")
+    private String productBacklogId;
+
+    @NotNull(message = "Priority is required")
+    private UserStoryPriority userStoryPriority;
+
+    @NotNull(message = "Status is required")
+    private UserStoryStatus userStoryStatus;
+}
